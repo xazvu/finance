5fc5df091e78ef173da9a1841aa03af699175cfe
erwin <isa732827@gmail.com>
2026-02-14 02:12:40 +0300

    start project

---
+++ main.py
@@ -0,0 +1,22 @@
+import asyncio
+import os
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
+
+
+bot = Bot(token=os.getenv('TOKEN'))
+
+dp = Dispatcher()
+
+
+async def main():
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+
+    # dp.update.middleware(DataBaseSession(session_pool=session_maker)) слой для сессии
+
+    logging.basicConfig(level=logging.INFO)
